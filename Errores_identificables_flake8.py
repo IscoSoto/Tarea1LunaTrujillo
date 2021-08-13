de7b6a2021c120cf08c432ea85eb85c286d630e6
Ricardo Luna <88362802+Rx4M00N@users.noreply.github.com>
2021-08-13 08:45:16 -0600

    Corrección de los errores en flake8.
    Se realizaron los ajustes correspondientes para que el código no presente errores en flake8.

--- Errores_identificables_flake8.py
+++ Errores_identificables_flake8.py
@@ -1,3 +1,5 @@
-#Parte 6 de la tarea.
-# "El mundo no puede ser cambiado solo con palabas bonitas. Para poder cambiar al mundo, uno tiene que estar dispuesto a ensuciarse las manos" - Lelouch vi Britannia.
-print(7==3)
+# Parte 6 de la tarea.
+# "El mundo no puede ser cambiado solo con palabas bonitas. 
+# Para poder cambiar al mundo, uno tiene que estar dispuesto a 
+# ensuciarse las manos" - Lelouch vi Britannia.
+print(7 == 3)
