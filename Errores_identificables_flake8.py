6e7c3b5deb72df90db8cf2b402774cf5f24e5489
IscoSoto <88363129+IscoSoto@users.noreply.github.com>
2021-08-13 08:42:49 -0600

    Archivo con errores de flake8.
    Corresponde al archivo de código con 3 errores identificables por flake8.

---
+++ Errores_identificables_flake8.py
@@ -0,0 +1,3 @@
+#Parte 6 de la tarea.
+# "El mundo no puede ser cambiado solo con palabas bonitas. Para poder cambiar al mundo, uno tiene que estar dispuesto a ensuciarse las manos" - Lelouch vi Britannia.
+print(7==3)
